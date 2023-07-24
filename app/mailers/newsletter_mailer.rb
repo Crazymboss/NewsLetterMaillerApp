0a737542cbf598458719ea0fa9ec58a851611b0a
Daniel <purdaioandaniel@gmail.com>
2023-07-24 17:10:29 +0300

    styled the code

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -1,31 +1,17 @@
 class NewsletterMailer < ApplicationMailer  
-  def email(subscriber, email, template_id=1)
+  def email(subscriber, email)
     @subscriber = subscriber
     @email = email
-
     mail to: @subscriber.email, subject: @email.subject do |format|
-      format.html { render layout: custom_layout(template_id) }
+      format.html { render layout: custom_layout }
     end
   end
 
-  def custom_layout(template_id)
+  def custom_layout
     template = Template.last
     name = template.name
     abs_path = Rails.root.join("app/views/layouts/#{name}.html.erb")
-
-    # Layout HTML.
-    # It could come from the DB, doesn't have to be hard coded.
-    html = template.body.html_safe
-    # html = <<~LAYOUT
-    # <h1>Asta va fi din db, numele va fi tot din db </h1>
-    # <%= yield %>
-    # <p>asta tot din db</p>
-    # LAYOUT
-
-    # Create a new layout file
-    File.write(abs_path, html)
-
-    # Return the layout name
+    File.write(abs_path, template.body.html_safe)
     name
   end
 end
--- app/models/template.rb
+++ app/models/template.rb
@@ -1,3 +1,4 @@
 class Template < ApplicationRecord
-
+    validates :name, presence: true
+    validates :body, presence: true
 end
